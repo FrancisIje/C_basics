ca38de0c80e6d645ef2d9029308a5e00c668b7d1
FrancisIje <ijenebefrancis@gmail.com>
2025-10-06 00:51:05 +0100

    integers add to new list

--- list.c
+++ list.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+// ineffectively adding new integers to fixed list
 int main(void)
 {
-    int list[3];
+    int *list = malloc(3 * sizeof(int));
+    if (list == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
-    for (int i = 0; i < 3; i++)
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
     {
-        printf("%i\n", i);
+        free(tmp);
+        return 1;
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        tmp[i] = list[i];
+    }
+
+    tmp[3] = 4;
+    free(list);
+
+    list = tmp;
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i\n", list[i]);
     };
 }
